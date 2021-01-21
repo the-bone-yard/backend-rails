01ed96dd8ebb3cf8d3f74265263dea0f81654d66
TravisM <mckinstrytg1@gmail.com>
2021-01-21 14:44:19 -0700

    Reduces rubocop offenses

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApiKey
   def self.generator
     SecureRandom.base64.tr('+/=', 'Qrt')
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password }
+  end
+
+  describe 'api_key' do
+    it 'api_key is generated and assigned upon creation' do
+      user = User.create!(email: 'hello@email.com', password: 'yep')
+
+      expect(user.api_key).to_not be_nil
+      expect(user.api_key).to be_a(String)
+      expect(user.api_key.length).to eq(24)
+    end
   end
 end

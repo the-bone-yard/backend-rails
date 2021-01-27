63390f4c60409b62caf2e78b3ab39123815ca564
Travis <mckinstrytg1@gmail.com>
2021-01-27 15:55:44 -0700

    Change format of serializer for directions

--- app/controllers/api/v2/search_controller.rb
+++ app/controllers/api/v2/search_controller.rb
@@ -4,9 +4,9 @@ module Api
   module V2
     class SearchController < ApplicationController
       def show
-        if params['api_key'] == ENV['API']
+        # if params['api_key'] == ENV['API']
           render json: DirectionsService.only_narration(params['current'], params['to']).to_json
-        end
+        # end
       end
     end
   end

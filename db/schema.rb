f84c55dfeff57e97b51947f6a162294754c04140
Travis <mckinstrytg1@gmail.com>
2021-01-27 12:32:07 -0700

    Creates welcome page

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,31 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_121_213_807) do
+ActiveRecord::Schema.define(version: 2021_01_21_213807) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'coordinates', force: :cascade do |t|
-    t.string 'city'
-    t.string 'area'
-    t.string 'lat'
-    t.string 'lng'
+  create_table "coordinates", force: :cascade do |t|
+    t.string "city"
+    t.string "area"
+    t.string "lat"
+    t.string "lng"
   end
 
-  create_table 'parks', force: :cascade do |t|
-    t.string 'name'
-    t.string 'formatted_address'
-    t.string 'opening_hours'
-    t.string 'photo'
-    t.string 'rating'
-    t.string 'email'
-    t.string 'lat'
-    t.string 'lng'
+  create_table "parks", force: :cascade do |t|
+    t.string "name"
+    t.string "formatted_address"
+    t.string "opening_hours"
+    t.string "photo"
+    t.string "rating"
+    t.string "email"
+    t.string "lat"
+    t.string "lng"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.string 'password_digest'
-    t.string 'api_key'
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "api_key"
   end
+
 end

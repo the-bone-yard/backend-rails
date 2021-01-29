d4a7ac2e4afb72a9ab277f779a898cc952e29816
Travis <mckinstrytg1@gmail.com>
2021-01-29 13:15:39 -0700

    Makes v2 for API

---
+++ app/controllers/api/v2/search_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  module V2
+    class SearchController < ApplicationController
+      def show
+        if params['api_key'] == ENV['API']
+          render json: DirectionsService.only_narration(params['current'], params['to']).to_json
+        end
+      end
+    end
+  end
+end

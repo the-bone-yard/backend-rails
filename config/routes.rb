4ce185874d4724cafcd87b0df4f8fb527cc95217
Travis <mckinstrytg1@gmail.com>
2021-02-02 11:02:54 -0700

    Small styling reformat for routes file

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       delete 'park', to: 'park#destroy'
       get 'park/all', to: 'park#index'
     end
+    
     namespace :v2 do
       get 'directions', to: 'search#show'
       get 'park_search', to: 'search#parks'
